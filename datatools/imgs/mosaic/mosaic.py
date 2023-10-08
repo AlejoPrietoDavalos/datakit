538809c3167843040f5bab7cafa2d975d32747bc
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-08 19:47:21 -0300

    mosaic y unet vamos a agregar

--- datatools/imgs/mosaic/mosaic.py
+++ datatools/imgs/mosaic/mosaic.py
@@ -2,13 +2,19 @@
 - TODO: Se podría crear una clase que genere y gestione una grilla de una imagen.
 - TODO: Se me ocurre una para imagenes generales y otra para rasters.
 """
+from __future__ import annotations
+__all__ = ["MosaicGenerator", "MosaicRaster"]
+
+import rasterio
 from rasterio import DatasetReader
 import numpy as np
 
+from pathlib import Path
+
 from pydantic import BaseModel, ConfigDict
 
+
 class MosaicRaster(BaseModel):
-    raster: DatasetReader
     
     @property
     def width(self) -> int:
@@ -19,4 +25,9 @@ class MosaicRaster(BaseModel):
 
 
 class MosaicGenerator:
-    pass
+    """ TODO: Con diferentes modos de generación, tiles de igual tamaño o de tamaño definido."""
+    def __init__(self, path_raster: Path, path_folder_out: Path):
+        self.raster: DatasetReader = rasterio.open(path_raster)
+        self.path_folder_out = path_folder_out
+    
+    #def 
