d16dc7f9f7fd5e3518385c7b9f209bf7816a2920
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-09 05:26:15 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "datatools",
+    version = "0.0.1",
+    packages = find_packages(),
+    install_requires = [
+        "numpy==1.26.0",
+        "pydantic==2.4.2",
+        "matplotlib==3.8.0",
+
+    ],
+)
