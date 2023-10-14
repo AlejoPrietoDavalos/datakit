81b1408195d5f6a7d0a5b7fed3a3d84b92bb2736
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-14 18:26:02 -0300

    Band RGB y Nir

---
+++ datatools/images/bands.py
@@ -0,0 +1,44 @@
+""" FIXME: Ver si se puede desacoplar el @field_validator y hacerlo genérico."""
+__all__ = ["BandsRGB", "BandsRGBNir"]
+from datatools.pydantic_base import BaseModelValAssign
+from datatools.validators import assert_ndarray_2D
+from datatools.rasters import RGBArr
+from datatools import T_NDArray
+
+from abc import ABC
+
+import numpy as np
+from pydantic import field_validator
+
+_RGB_BANDS = ("red", "green", "blue")
+
+
+class BandsBase(BaseModelValAssign, ABC):
+    pass
+
+
+class BandsRGB(BandsBase):
+    """ TODO: Poner condicional de len(shape)==2."""
+    red: T_NDArray
+    green: T_NDArray
+    blue: T_NDArray
+
+    @property
+    def rgb(self) -> RGBArr:
+        """ Array 3D con las bandas stackeadas."""
+        return np.stack([self.red, self.green, self.blue], axis=-1)
+
+    @field_validator(*_RGB_BANDS)
+    def _val_bands(cls, arr: np.ndarray) -> np.ndarray:
+        assert_ndarray_2D(arr)
+        return arr
+
+
+class BandsRGBNir(BandsRGB):
+    """ TODO: Todo lo que se puede calcular con estas bandas."""
+    nir: T_NDArray
+
+    @field_validator(*_RGB_BANDS, "nir")
+    def _val_bands(cls, arr: np.ndarray) -> np.ndarray:
+        assert_ndarray_2D(arr)
+        return arr
