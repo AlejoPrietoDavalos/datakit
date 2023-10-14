abc68ca6e22e4af84925c8e02a580142855fa248
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-14 10:55:02 -0300

    draft

--- _draft/path_mejorar.py
+++ _draft/path_mejorar.py
@@ -1,19 +1,25 @@
+from pydantic import BaseModel
+
 from pathlib import Path
 
 from functools import cached_property
 
-import attr
-from attr.validators import instance_of
 
-@attr.s(frozen=True)
-class FileStructDS:
-    root: Path = attr.ib(validator=instance_of(Path))
+class FileStructDS(BaseModel):
+    root: Path
 
     @cached_property
-    def data(self) -> Path: return self.root / "data"
+    def data(self) -> Path:
+        return self.root / "data"
+    
     @cached_property
-    def raw(self) -> Path: return self.data / "raw"
+    def raw(self) -> Path:
+        return self.data / "raw"
+    
     @cached_property
-    def preprocess(self) -> Path: return self.data / "preprocess"
+    def preprocess(self) -> Path:
+        return self.data / "preprocess"
+    
     @cached_property
-    def processed(self) -> Path: return self.data / "processed"
+    def processed(self) -> Path:
+        return self.data / "processed"
