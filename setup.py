ca2960da9fa61c1f511a6333ced32e55bf1aa7ff
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-11-25 04:52:15 -0300

    setup

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ setup(
     packages = find_packages(),
     install_requires = [
         "numpy==1.26.0",
-        "pydantic==2.4.2",
+        "pydantic==2.5.2",
         "matplotlib==3.8.0",
         "rasterio==1.3.8",
         "multipledispatch==1.0.0",
-        "pydantic==2.5.2",
         "torch",
         "torchvision",
     ],
