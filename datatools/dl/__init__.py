d5bd79a882124d90880285e5bc041546149c036c
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-02 18:51:16 -0300

    common types

---
+++ datatools/dl/__init__.py
@@ -0,0 +1,2 @@
+from .configs import CfgModuleBase, CfgModule, CfgConv2d
+from .common_types import _size_2_t, T_Dump
---
+++ datatools/dl/common_types.py
@@ -0,0 +1,4 @@
+from torch.nn.common_types import _size_2_t
+from typing import Dict, Any
+
+T_Dump = Dict[str, Any]
