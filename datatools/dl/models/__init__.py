6f388b6323a53ce614147ae2723ee4e5646d2999
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-02 18:49:29 -0300

    models

---
+++ datatools/dl/models/__init__.py
@@ -0,0 +1,4 @@
+"""
+Aquí irán los modelos a instanciar listo para entrenar, éstos
+deben combinar elementos dentro de `modules` para crear el modelo.
+"""
---
+++ datatools/dl/models/unet/__init__.py
@@ -0,0 +1,1 @@
+""" U-NET Model, semantic segmentation. https://arxiv.org/pdf/1505.04597.pdf"""
