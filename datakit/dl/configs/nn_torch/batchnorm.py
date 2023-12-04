5b7bc6281b0b7a7f3247fc745bfce85bf29de687
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-04 16:39:49 -0300

    nn_torch

---
+++ datakit/dl/configs/nn_torch/batchnorm.py
@@ -0,0 +1,18 @@
+from ..module import CfgModuleDumpable
+from torch import nn
+
+from pydantic import NonNegativeInt, PositiveFloat
+from typing import Type
+
+__all__ = ["CfgBatchNorm2d"]
+
+class CfgBatchNorm2d(CfgModuleDumpable):
+    num_features: NonNegativeInt
+    eps: PositiveFloat = 1e-5
+    momentum: PositiveFloat = 0.1
+    affine: bool = True
+    track_running_stats: bool = True
+
+    @property
+    def module_cls(self) -> Type[nn.BatchNorm2d]:
+        return nn.BatchNorm2d
---
+++ datakit/dl/configs/nn_torch/conv.py
@@ -0,0 +1,57 @@
+from ..module import CfgModuleDumpable
+
+from torch import nn
+
+from pydantic import NonNegativeInt
+from datakit.dl.common_types import _size_2_t
+from typing import Type
+
+
+__all__ = ["CfgConv2d"]
+
+
+from enum import StrEnum, auto
+class PaddingMode(StrEnum):
+    """ TODO: Testear"""
+    zeros = auto()
+    reflect = auto()
+    replicate = auto()
+    circular = auto()
+
+    @classmethod
+    def default(cls) -> str:
+        return cls.zeros.value
+
+
+class CfgConv2d(CfgModuleDumpable):
+    in_channels: NonNegativeInt
+    out_channels: NonNegativeInt
+    kernel_size: _size_2_t
+    stride: _size_2_t = 1
+    padding: _size_2_t | str = 0
+    dilation: _size_2_t = 1
+    groups: NonNegativeInt = 1
+    bias: bool = True
+    padding_mode: PaddingMode = PaddingMode.default()
+
+    @property
+    def module_cls(self) -> Type[nn.Conv2d]:
+        return nn.Conv2d
+
+
+class CfgConvTranspose2d(CfgModuleDumpable):
+    in_channels: NonNegativeInt
+    out_channels: NonNegativeInt
+    kernel_size: _size_2_t
+    stride: _size_2_t = 1
+    padding: _size_2_t = 0
+    output_padding: _size_2_t = 0
+    groups: NonNegativeInt = 1
+    bias: bool = True
+    dilation: _size_2_t = 1
+    padding_mode: PaddingMode = PaddingMode.default()
+
+    @property
+    def module(self) -> Type[nn.ConvTranspose2d]:
+        return nn.ConvTranspose2d
+
