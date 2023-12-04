6ffcf0c4c0cb5b0feb715fbb80e12659fad10dc8
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-04 11:15:02 -0300

    common_types de PyTorch mejorado con Pydantic para validacion de enteros positivos

---
+++ datakit/dl/common_types.py
@@ -0,0 +1,46 @@
+""" Versión usando `Pydantic` de `torch.nn.common_types` de `PyTorch`."""
+from torch.nn.common_types import (
+    _scalar_or_tuple_any_t,
+    _scalar_or_tuple_1_t,
+    _scalar_or_tuple_2_t,
+    _scalar_or_tuple_3_t,
+    _scalar_or_tuple_4_t,
+    _scalar_or_tuple_5_t,
+    _scalar_or_tuple_6_t
+)
+#from torch import Tensor
+
+from pydantic import NonNegativeInt
+#from typing import Optional
+
+__all__ = ["_size_any_t",
+           "_size_1_t", "_size_2_t", "_size_3_t",
+           "_size_4_t", "_size_5_t", "_size_6_t"]
+
+
+# For arguments which represent size parameters (eg, kernel size, padding)
+_size_any_t = _scalar_or_tuple_any_t[NonNegativeInt]
+_size_1_t = _scalar_or_tuple_1_t[NonNegativeInt]
+_size_2_t = _scalar_or_tuple_2_t[NonNegativeInt]
+_size_3_t = _scalar_or_tuple_3_t[NonNegativeInt]
+_size_4_t = _scalar_or_tuple_4_t[NonNegativeInt]
+_size_5_t = _scalar_or_tuple_5_t[NonNegativeInt]
+_size_6_t = _scalar_or_tuple_6_t[NonNegativeInt]
+
+# For arguments which represent optional size parameters (eg, adaptive pool parameters)
+#_size_any_opt_t = _scalar_or_tuple_any_t[Optional[NonNegativeInt]]
+#_size_2_opt_t = _scalar_or_tuple_2_t[Optional[NonNegativeInt]]
+#_size_3_opt_t = _scalar_or_tuple_3_t[Optional[NonNegativeInt]]
+
+# For arguments that represent a ratio to adjust each dimension of an input with (eg, upsampling parameters)
+#_ratio_2_t = _scalar_or_tuple_2_t[float]
+#_ratio_3_t = _scalar_or_tuple_3_t[float]
+#_ratio_any_t = _scalar_or_tuple_any_t[float]
+
+#_tensor_list_t = _scalar_or_tuple_any_t[Tensor]
+
+# For the return value of max pooling operations that may or may not return indices.
+# With the proposed 'Literal' feature to Python typing, it might be possible to
+# eventually eliminate this.
+#_maybe_indices_t = _scalar_or_tuple_2_t[Tensor]
+
