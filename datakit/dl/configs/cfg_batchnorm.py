63657e8988ab7d90fbc36ce98722c4fcde64d5bc
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-04 12:09:07 -0300

    pydantic types para BatchNorm2d

--- datakit/dl/configs/cfg_batchnorm.py
+++ datakit/dl/configs/cfg_batchnorm.py
@@ -1,13 +1,13 @@
 from .cfg_module import CfgModuleDumpable
 from torch import nn
 
-from pydantic import PositiveFloat
+from pydantic import NonNegativeInt, PositiveFloat
 from typing import Type
 
 __all__ = ["CfgBatchNorm2d"]
 
 class CfgBatchNorm2d(CfgModuleDumpable):
-    num_features: int
+    num_features: NonNegativeInt
     eps: PositiveFloat = 1e-5
     momentum: PositiveFloat = 0.1
     affine: bool = True
