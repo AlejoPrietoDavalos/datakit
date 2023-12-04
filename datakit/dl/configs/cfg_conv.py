2d1ea4cff15d7d4a07b0326e6ee463d54b13d601
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-04 10:48:39 -0300

    CfgConv2d

--- datakit/dl/configs/cfg_conv.py
+++ datakit/dl/configs/cfg_conv.py
@@ -1,21 +1,28 @@
+from typing import Type
 from .cfg_module import CfgModuleDumpable
 
-from torch.nn.common_types import _size_2_t
+from torch import nn
+from torch.nn.common_types import _scalar_or_tuple_2_t
 from pydantic import NonNegativeInt
+_size_2_t = _scalar_or_tuple_2_t[NonNegativeInt]
 
 __all__ = ["CfgConv2d"]
+
 
 class CfgConv2d(CfgModuleDumpable):
     in_channels: NonNegativeInt
     out_channels: NonNegativeInt
     kernel_size: _size_2_t
     stride: _size_2_t = 1
-    padding: _size_2_t | str = NonNegativeInt
+    padding: _size_2_t | str = 0
     dilation: _size_2_t = 1
     groups: int = 1
     bias: bool = True
     padding_mode: str = 'zeros'
 
+    @property
+    def module_cls(self) -> Type[nn.Conv2d]:
+        return nn.Conv2d
 
 class CfgConv2dNorm:
     pass
--- datakit/dl/configs/cfg_module.py
+++ datakit/dl/configs/cfg_module.py
@@ -1,8 +1,10 @@
 from datakit.dl import FnAct
 
+from torch import nn
+
 from pydantic import BaseModel, Field, ConfigDict
-from abc import ABC
-from typing import List, Dict, Any
+from abc import ABC, abstractproperty
+from typing import Generic, TypeVar, Type, List, Dict, Any
 
 __all__ = ["CfgModuleDumpable", "CfgModule"]
 
@@ -13,11 +15,21 @@ class _CfgModuleBase(BaseModel, ABC):
     model_config = ConfigDict(validate_assignment=True, frozen=True)
 
 
-class CfgModuleDumpable(_CfgModuleBase):
+class CfgModuleDumpable(_CfgModuleBase, ABC):
     @property
     def exclude_module_dump(self) -> List[str]:
         """ Elementos a ignorar dentro de `module_dump`."""
         return []
+    
+    @property
+    def module(self) -> nn.Module:
+        """ Instancia el modelo asociado a esta configuración."""
+        return self.module_cls(**self.module_dump())
+    
+    @abstractproperty
+    def module_cls(self) -> Type[nn.Module]:
+        """ Debe retornar la clase que usará para instanciar."""
+        ...
     
     def module_dump(self) -> T_Dump:
         """ Utilizar para instanciar los módulos."""
