5c25bc95efca4c843bf10cd034dfd91899214cb5
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-10 20:42:21 -0300

    safe_divide

--- datakit/rasters/indexes.py
+++ datakit/rasters/indexes.py
@@ -3,56 +3,45 @@ __all__ = ["ndvi"]
 from .typings import BandArr
 import numpy as np
 
-from typing import Callable, Tuple
 
-def wrapper_div_zero(fn_calc_index: Callable[[np.ndarray], np.ndarray]) -> Callable:
-    def wrapper(*args, **kwargs):
-        with np.errstate(divide='ignore', invalid='ignore'):
-            index_arr = fn_calc_index(*args, **kwargs)
-            index_arr[~np.isfinite(index_arr)] = 0
-        return index_arr
-    return wrapper
+def safe_divide(numerator: np.ndarray, denominator: np.ndarray, default_value=0) -> np.ndarray:
+    with np.errstate(divide='ignore', invalid='ignore'):
+        result = np.divide(numerator, denominator)
+        result[~np.isfinite(result)] = default_value
+    return result
 
-@wrapper_div_zero
+
 def ndvi(red: BandArr, nir: BandArr) -> BandArr:
     """ Normalized Difference Vegetation Index."""
-    ndvi_arr = (nir - red) / (nir + red)
-    return ndvi_arr
+    return safe_divide(nir - red, nir + red)
 
-@wrapper_div_zero
 def ndwi(nir: BandArr, swir: BandArr) -> BandArr:
     """ Normalized Difference Water Index."""
-    return (nir - swir) / (nir + swir)
+    return safe_divide(nir - swir, nir + swir)
 
-@wrapper_div_zero
 def ndbi(nir: BandArr, swir: BandArr) -> BandArr:
     """ Normalized Difference Built-up Index."""
-    return (swir - nir) / (swir + nir)
+    return safe_divide(swir - nir, swir + nir)
 
-@wrapper_div_zero
 def ndmi(nir: BandArr, swir: BandArr) -> BandArr:
     """ Normalized Difference Moisture Index."""
-    return (nir - swir) / (nir + swir)
+    return safe_divide(nir - swir, nir + swir)
 
-@wrapper_div_zero
 def savi(nir: BandArr, red: BandArr, L=0.5) -> BandArr:
     """ Soil-Adjusted Vegetation Index."""
     assert 0<=L<=1
-    return ((nir - red) / (nir + red + L)) * (1 + L)
+    return (1 + L) * safe_divide(nir - red, nir + red + L)
 
-@wrapper_div_zero
 def evi(nir: BandArr, red: BandArr, blue: BandArr, L=1, C1=6, C2=7.5, G=2.5) -> BandArr:
     """ Enhanced Vegetation Index.
     - TODO: Revisar esos hiperparámetros."""
-    return G * ((nir - red) / (nir + C1 * red - C2 * blue + L))
+    return G * safe_divide(nir - red, nir + C1 * red - C2 * blue + L)
 
-@wrapper_div_zero
 def mndwi(green: BandArr, swir: BandArr) -> BandArr:
     """ Modified Normalized Difference Water Index."""
-    return (green - swir) / (green + swir)
+    return safe_divide(green - swir, green + swir)
 
-@wrapper_div_zero
 def bai(nir: BandArr, swir: BandArr) -> BandArr:
     """ Burned Area Index."""
-    #return (1 / ((0.1 - nir) ** 2 + (0.06 - swir) ** 2)) ** 0.5
+    ###return (1 / ((0.1 - nir) ** 2 + (0.06 - swir) ** 2)) ** 0.5
     raise NotImplementedError("Buscar bien este indice.")
